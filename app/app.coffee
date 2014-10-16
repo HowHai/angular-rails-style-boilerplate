15a48012718a64a669758a92b7f2b9f1a4221419
Hai Nguyen <hmn91733@gmail.com>
2014-10-16 11:47:57 -0700

    Clean up index.html

---
+++ app/app.coffee
@@ -0,0 +1,12 @@
+# Vendors
+vendors = [
+  'ui.router',
+  'restangular'
+]
+
+main_vendors = vendors.concat([
+])
+
+# Services
+
+app = angular.module "MainApp", main_vendors
--- app/app.js
+++ app/app.js
@@ -1,1 +1,7 @@
+var app, main_vendors, vendors;
 
+vendors = ['ui.router', 'restangular'];
+
+main_vendors = vendors.concat([]);
+
+app = angular.module("MainApp", main_vendors);
